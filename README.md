784c81088c2ef158bfd647283a089c39f8ceaccb
h0ngg0m <jaehongbyeon98@gmail.com>
2024-12-11 15:43:17 +0900

    :white_check_mark: @Value

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## 스프링이 지원하는 다양한 외부 설정 조회 방법
 - `Environment` (`MyDataSourceEnvConfig`)
-- `@Value` - 값 주입
+  - 단점: `Environment`를 직접 주입받고, `environment.getProperty(key)`를 통해서 값을 꺼내는 과정을 반복해야 한다는 점.
+- `@Value` - 값 주입 (`MyDataSourceValueConfig`)
+  - `@Value`도 내부적으로는 `Environment`를 사용하고 있음.
+  - 단점: `@Value`로 하나하나 외부 설정 정보의 키 값을 입력받고, 주입 받아와야 하는 부분이 번거로움.
 - `@ConfigurationProperties` - 타입 안전한 설정 속성
--- src/main/java/com/honggom/springbootexternalread/SpringBootExternalReadApplication.java
+++ src/main/java/com/honggom/springbootexternalread/SpringBootExternalReadApplication.java
@@ -1,12 +1,14 @@
 package com.honggom.springbootexternalread;
 
 import com.honggom.springbootexternalread.config.MyDataSourceEnvConfig;
+import com.honggom.springbootexternalread.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 
-@Import(MyDataSourceEnvConfig.class)
+//@Import(MyDataSourceEnvConfig.class)
+@Import(MyDataSourceValueConfig.class)
 @SpringBootApplication(scanBasePackages = "com.honggom.springbootexternalread.datasource")
 public class SpringBootExternalReadApplication {
 
